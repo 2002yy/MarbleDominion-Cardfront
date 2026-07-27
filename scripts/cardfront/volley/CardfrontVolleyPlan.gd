0c580e9dddcbfbd954401d19dda9024e5a1a559a
张元垚 <146716153+2002yy@users.noreply.github.com>
2026-07-27 19:33:08 +0800

    feat: expose segmented volley plan data

--- scripts/cardfront/volley/CardfrontVolleyPlan.gd
+++ scripts/cardfront/volley/CardfrontVolleyPlan.gd
@@ -7,6 +7,10 @@ var projectile_power: int = 1
 var projectile_sequence: Array = []
 var projectile_counts: Dictionary = {}
 var projectile_conversions_applied: Dictionary = {}
+var special_projectile_sequence: Array = []
+var standard_projectile_sequence: Array = []
+var special_shot_count: int = 0
+var standard_shot_count: int = 0
 var attack_level: int = 0
 var chamber_damage_quarters: int = 4
 var armor_pierce_contacts: int = 0
@@ -26,6 +30,10 @@ func snapshot() -> Dictionary:
 		"projectile_sequence": projectile_sequence.duplicate(),
 		"projectile_counts": projectile_counts.duplicate(true),
 		"projectile_conversions_applied": projectile_conversions_applied.duplicate(true),
+		"special_projectile_sequence": special_projectile_sequence.duplicate(),
+		"standard_projectile_sequence": standard_projectile_sequence.duplicate(),
+		"special_shot_count": special_shot_count,
+		"standard_shot_count": standard_shot_count,
 		"attack_level": attack_level,
 		"chamber_damage_quarters": chamber_damage_quarters,
 		"armor_pierce_contacts": armor_pierce_contacts,
